c94171e04b0f81b3d9b4f165ae02463591490e75
Vlad Borisov <vladborisov182@gmail.com>
2017-11-14 21:00:24 +0200

    edit

--- testModule.py
+++ testModule.py
@@ -16,7 +16,7 @@ class stringToListTest(unittest.TestCase):
         self.assertEqual(calc.Calculator.stringToList(self, "2007+30"), ["2007", "+", "30"])
     
     def test_fullEquation(self):
-        self.assertEqual(calc.Calculator.stringToList(self, "2+2*2/(30-5)+2"), ["2", "+", "2", "*",                                               "2", "/", "(", "30", "-", "5", ")", "+", "2", ])
+        self.assertEqual(calc.Calculator.stringToList(self, "2+2*2/(30-5)+2"), ["2", "+", "2", "*", "2", "/", "(", "30", "-", "5", ")", "+", "2", ])
 
 
 class infixToPostfixTest(unittest.TestCase):
@@ -28,13 +28,13 @@ class infixToPostfixTest(unittest.TestCase):
         self.assertEqual(calc.Calculator.infixToPostfix(self, ["2", "+", "2"]), ["2", "2", "+"])
     
     def test_bracketedEquation(self):
-        self.assertEqual(calc.Calculator.infixToPostfix(self, ["(", "2", "+", "2", ")", "*", "10"]),                                                        ["2", "2", "+", "10", "*"])
+        self.assertEqual(calc.Calculator.infixToPostfix(self, ["(", "2", "+", "2", ")", "*", "10"]), ["2", "2", "+", "10", "*"])
     
     def test_extraParentheses(self):
-        self.assertEqual(calc.Calculator.infixToPostfix(self, ["2", "+", "(", "(", "2", "*", "2", ")"]                                                ), ["2", "2", "2", "*", "+"])
+        self.assertEqual(calc.Calculator.infixToPostfix(self, ["2", "+", "(", "(", "2", "*", "2", ")"]), ["2", "2", "2", "*", "+"])
     
     def test_permissibleError(self):
-        self.assertEqual(calc.Calculator.infixToPostfix(self, ["2", "+", "2", "*", "2", "+", "("]),                                                    ["2", "2", "2", "*", "(", "+", "+"])
+        self.assertEqual(calc.Calculator.infixToPostfix(self, ["2", "+", "2", "*", "2", "+", "("]), ["2", "2", "2", "*", "(", "+", "+"])
 
 
 class reversedPolishNotationTest(unittest.TestCase):
@@ -52,8 +52,7 @@ class reversedPolishNotationTest(unittest.TestCase):
         self.assertEqual(calc.Calculator.reversedPolishNotation(self, ["2", "10", "+", "4", "5", "/", "3", "+", "-"]), 8.2)
 
     def test_permissibleError(self):
-        self.assertEqual(calc.Calculator.reversedPolishNotation(self, ["2", "2", "2", "*", "(", 
-                                                                     "+", "+"]), "Мало операндов")
+        self.assertEqual(calc.Calculator.reversedPolishNotation(self, ["2", "2", "2", "*", "(", "+", "+"]), "Мало операндов")
 
         
 
